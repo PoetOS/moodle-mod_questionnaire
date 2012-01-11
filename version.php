7fdbfda7a6dae8992e4ccbcb53a697aef81b1dad
Mike Churchward <mike@remote-learner.net>
2012-01-11 13:34:28 -0500

    Added 'maturity' string.

--- version.php
+++ version.php
@@ -6,8 +6,9 @@
 /////////////////////////////////////////////////////////////////////////////////
 
 $module->version  = 2010110101;  // The current module version (Date: YYYYMMDDXX)
-$module->release  = '2.2.1 (Build - 20120106)';
 $module->requires = 2010112400;  // Requires this Moodle version
 $module->cron     = 60*60*12;    // Period for cron to check this module (secs)
 
+$module->release  = '2.2.1 (Build - 20120106)';
+$module->maturity = 'STABLE';
 ?>
