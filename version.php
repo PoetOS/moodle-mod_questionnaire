40d0811a0a856361a44dbe4ba24c4fc44951600a
Mike Churchward <mike@remote-learner.net>
2012-01-13 10:10:38 -0500

    Standardizing release tag for each branch of Moodle.

--- version.php
+++ version.php
@@ -9,4 +9,6 @@ $module->version  = 2008060405;  // The current module version (Date: YYYYMMDDXX
 $module->requires = 2007101509;  // Requires this Moodle version
 $module->cron     = 60*60*12;    // Period for cron to check this module (secs)
 
+$module->release  = '1.9.1 (Build - 20120106)';
+$module->maturity = 'STABLE';
 ?>
