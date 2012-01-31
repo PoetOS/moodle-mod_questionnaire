97df50bc9b6cab8bdd2299ad9dc9d50676d42785
Mike Churchward <mike@remote-learner.net>
2012-01-31 12:03:49 -0500

    CONTRIB-3044 - Fixing my version.php screw-up.

--- version.php
+++ version.php
@@ -5,8 +5,8 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2011070100;  // The current module version (Date: YYYYMMDDXX)
-$module->requires = 2010112400;  // Requires this Moodle version
+$module->version  = 2010110101;  // The current module version (Date: YYYYMMDDXX)
+$module->requires = 2011070100;  // Requires this Moodle version
 $module->cron     = 60*60*12;    // Period for cron to check this module (secs)
 
 $module->release  = '2.1.2 (Build - 20120131)';
