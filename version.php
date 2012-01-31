ba9927e30f766bb39b7214181ab6792fceb8f05a
Mike Churchward <mike@remote-learner.net>
2012-01-31 10:56:26 -0500

    CONTRIB-3044 - Bumped release information for release to community.

--- version.php
+++ version.php
@@ -9,5 +9,5 @@ $module->version  = 2010110101;  // The current module version (Date: YYYYMMDDXX
 $module->requires = 2010112400;  // Requires this Moodle version
 $module->cron     = 60*60*12;    // Period for cron to check this module (secs)
 
-$module->release  = '2.1.1 (Build - 20120106)';
+$module->release  = '2.1.2 (Build - 20120131)';
 $module->maturity = 'STABLE';
