9d0ce0e1d3480431504ae46cf4fdccf139bca15c
Mike Churchward <mike@remote-learner.net>
2012-01-31 11:40:44 -0500

    CONTRIB-3044 - Set requires information to M2.1.

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2010110101;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2011070100;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2010112400;  // Requires this Moodle version
 $module->cron     = 60*60*12;    // Period for cron to check this module (secs)
 
