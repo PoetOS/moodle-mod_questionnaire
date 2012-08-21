dbc2383808e86ee50cf12bd60e8a7406a1562a19
Davo Smith <github@davosmith.co.uk>
2012-08-21 17:35:13 +0200

    Fixed 'undefined variable usersql' warning

--- lib.php
+++ lib.php
@@ -337,6 +337,7 @@ function questionnaire_grades($questionnaireid) {
 function questionnaire_get_user_grades($questionnaire, $userid=0) {
     global $DB;
     $params = array();
+    $usersql = '';
     if (!empty($userid)) {
         $usersql = "AND u.id = ?";
         $params[] = $userid;
