c35ddd5a7e795285b941db7ea94cad22a1254a31
Jenny Gray <j.m.gray@open.ac.uk>
2012-10-08 10:28:35 +0100

    CONTRIB-3930 Extend survey name to match questionnaire name

--- db/upgrade.php
+++ db/upgrade.php
@@ -322,6 +322,29 @@ function xmldb_questionnaire_upgrade($oldversion=0) {
         upgrade_mod_savepoint(true, 2010110101, 'questionnaire');
     }
 
+    if ($oldversion < 2012100800) {
+        // Changing precision of field name on table questionnaire_survey to (255).
+
+        // First drop the index.
+        $table = new xmldb_table('questionnaire_survey');
+        $index = new xmldb_index('name');
+        $index->set_attributes(XMLDB_INDEX_NOTUNIQUE, array('name'));
+        $dbman->drop_index($table, $index);
+
+        // Launch change of precision for field name.
+        $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '255', null, XMLDB_NOTNULL, null, null, 'id');
+        $dbman->change_field_precision($table, $field);
+
+        // Add back in the index.
+        $table = new xmldb_table('questionnaire_survey');
+        $index = new xmldb_index('name');
+        $index->set_attributes(XMLDB_INDEX_NOTUNIQUE, array('name'));
+        $dbman->add_index($table, $index);
+
+        // questionnaire savepoint reached
+        upgrade_mod_savepoint(true, 2012100800, 'questionnaire');
+    }
+
     return $result;
 }
 
