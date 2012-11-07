8f0d8efe6d603e057e79f50480c69c62ad383ea7
Mike Churchward <mike@remote-learner.net>
2012-11-07 12:45:30 -0500

    CONTRIB-4005 - Fixed wrong API usage from 1.9 to 2.2.

--- version.php
+++ version.php
@@ -5,9 +5,9 @@
 ///  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 /////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2010110101;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2012110701;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2011070100;  // Requires this Moodle version
+$module->maturity = MATURITY_STABLE;
 $module->cron     = 60*60*12;    // Period for cron to check this module (secs)
 
-$module->release  = '2.1.3 (Build - 20120511)';
-$module->maturity = 'STABLE';
+$module->release  = '2.1.4 (Build - 20121107)';
