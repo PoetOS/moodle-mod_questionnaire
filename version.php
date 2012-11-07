4d471ff57d5fd50118acffbf8e2ee5e0372f068b
Mike Churchward <mike@remote-learner.net>
2012-11-07 13:19:51 -0500

    Moved maturity string for 2.2.

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 $module->version  = 2012110701;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2011120500;  // Requires this Moodle version
+$module->maturity = 'STABLE';
 $module->component = 'mod_questionnaire';
 $module->cron     = 60*60*12;    // Period for cron to check this module (secs)
 
 $module->release  = '2.2.4 (Build - 20120711)';
-$module->maturity = 'STABLE';
