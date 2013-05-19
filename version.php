062ba8e7918101f1de49b65e7238fd4b9710ea49
Joseph Rézeau <moodle@rezeau.org>
2013-05-19 22:48:29 +0200

    CONTRIB-4314 :: In accordance with current Moodle languages policy, all language folders other than English have been removed from the lang folder. All translations are now available from AMOS.
    Signed-off-by: Joseph Rézeau <moodle@rezeau.org>

--- version.php
+++ version.php
@@ -22,10 +22,10 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version  = 2012110701;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2013051901;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2012062500;  // Requires this Moodle version
 $module->component = 'mod_questionnaire';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 60*60*12;    // Period for cron to check this module (secs)
 
-$module->release  = '2.3.3 (Build - 20121107)';
+$module->release  = '2.3.4 (Build - 20130519)';
