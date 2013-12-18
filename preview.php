55dc2b93def7f9dbea4c6ab6d80dfea3aa6e6cb6
Joseph Rézeau <moodle@rezeau.org>
2013-12-18 10:22:03 +0100

    CONTRIB-4800 :: Enable teachers to preview template or public questionnaires located in courses where they are not enrolled
    Signed-off-by: Joseph Rézeau <moodle@rezeau.org>

--- preview.php
+++ preview.php
@@ -60,7 +60,11 @@ if ($id) {
 }
 
 // Check login and get context.
-require_login($course->id, false, $cm);
+// Do not require login if this questionnaire is viewed from the Add questionnaire page
+// to enable teachers to view template or public questionnaires located in a course where they are not enroled.
+if (!$popup) {
+    require_login($course->id, false, $cm);
+}
 $context = $cm ? context_module::instance($cm->id) : false;
 
 $url = new moodle_url('/mod/questionnaire/preview.php');
@@ -72,9 +76,7 @@ if ($sid) {
 }
 $PAGE->set_url($url);
 
-if (!$popup) {
-    $PAGE->set_context($context);
-}
+$PAGE->set_context($context);
 
 $questionnaire = new questionnaire($qid, $questionnaire, $course, $cm);
 $owner = (trim($questionnaire->survey->owner) == trim($course->id));
@@ -82,7 +84,7 @@ $owner = (trim($questionnaire->survey->owner) == trim($course->id));
 $canpreview = (!isset($questionnaire->capabilities) &&
                has_capability('mod/questionnaire:preview', context_course::instance($course->id))) ||
               (isset($questionnaire->capabilities) && $questionnaire->capabilities->preview && $owner);
-if (!$canpreview) {
+if (!$canpreview && !$popup) {
     // Should never happen, unless called directly by a snoop...
     print_error('nopermissions', 'questionnaire', $CFG->wwwroot.'/mod/questionnaire/view.php?id='.$cm->id);
 }
@@ -111,7 +113,9 @@ $PAGE->requires->js('/mod/questionnaire/module.js');
 
 
 echo $OUTPUT->header();
-require('tabs.php');
+if (!$popup) {
+    require('tabs.php');
+}
 echo $OUTPUT->heading($pq);
 $questionnaire->survey_print_render('', 'preview', $course->id, $rid = 0, $popup);
 if ($popup) {
