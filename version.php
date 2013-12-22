af571e820acef34f4aa0c3bd167585751128d4b9
Joseph Rézeau <moodle@rezeau.org>
2013-12-22 17:25:59 +0100

    Version 2.6.2 (Build - 2013122200) BETA released to the Moodle Plugins Directory.
    Signed-off-by: Joseph Rézeau <moodle@rezeau.org>

--- version.php
+++ version.php
@@ -22,11 +22,11 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version  = 2013120200;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2013122200;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2013111800; // Moodle version.
 
 $module->component = 'mod_questionnaire';
 $module->cron     = 60 * 60 * 12;    // Period for cron to check this module (secs).
 
-$module->release  = '2.6.1 (Build - 2013120200)';
+$module->release  = '2.6.2 (Build - 2013122200)';
 $module->maturity  = MATURITY_BETA;
