cb754e1e24d62b2b8893e0a634880185f3800d2b
Joseph Rézeau <moodle@rezeau.org>
2013-12-22 16:47:47 +0100

    Version 2.5.8 Build - 2013122200 ready for Moodle plugins Directory.
    Signed-off-by: Joseph Rézeau <moodle@rezeau.org>

--- version.php
+++ version.php
@@ -22,10 +22,10 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version = 2013120200;  // The current module version (Date: YYYYMMDDXX)
+$module->version = 2013122200;  // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2013051400;  // Requires this Moodle version.
 $module->component = 'mod_questionnaire';
 $module->cron = 60 * 60 * 12;    // Period for cron to check this module (secs).
 
-$module->release = '2.5.7 (Build - 2013120200)';
+$module->release = '2.5.8 (Build - 2013122200)';
 $module->maturity = MATURITY_STABLE;
