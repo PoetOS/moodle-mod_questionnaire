7a3154eb2cd9c55517737d941e3b7e90755814be
Mike Churchward <mike@remote-learner.net>
2015-03-19 14:01:49 -0400

    Releasing 2.8.2

--- version.php
+++ version.php
@@ -25,11 +25,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2014111001;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2014111002;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2014111000; // Moodle version.
 
 $plugin->component = 'mod_questionnaire';
 $plugin->cron     = 60 * 60 * 12;    // Period for cron to check this module (secs).
 
-$plugin->release  = '2.8.1 (Build - 2015020601)';
+$plugin->release  = '2.8.2 (Build - 2015031901)';
 $plugin->maturity  = MATURITY_STABLE;
