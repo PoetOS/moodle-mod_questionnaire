c511ed6e3b03dbfdbcc2d561d0e97a55fab5e63d
Mike Churchward <mike@remote-learner.net>
2015-06-12 11:40:45 -0400

    CONTRIB-5737 - Fixing version file before merge into MOODLE_29_STABLE.

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2015051100;  // The current module version (Date: YYYYMMDDXX)
-$plugin->requires = 2015061000; // Moodle version.
+$plugin->version  = 2015051101;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2015051100; // Moodle version.
 
 $plugin->component = 'mod_questionnaire';
 
