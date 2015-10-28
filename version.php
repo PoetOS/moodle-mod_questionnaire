66f9e9e55741f06802b9741aa3b9a40a6875aaf5
Mike Churchward <mike@remote-learner.net>
2015-10-28 11:44:19 -0400

    Creating beta 3.0 release.

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2015051102;  // The current module version (Date: YYYYMMDDXX)
-$plugin->requires = 2015051100; // Moodle version.
+$plugin->version  = 2015102800;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2015102800; // Moodle version.
 
 $plugin->component = 'mod_questionnaire';
 
-$plugin->release  = '2.9.0 (Build - 2015060701)';
-$plugin->maturity  = MATURITY_STABLE;
+$plugin->release  = '3.0beta (Build - 2015102800)';
+$plugin->maturity  = MATURITY_BETA;
