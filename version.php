b4e603a4c1179bf35506b55063165787383f8c33
Mike Churchward <mike@remote-learner.net>
2016-07-07 15:47:04 -0400

    Setting up 3.1.1 release to fix two nasty bugs.

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2016020201;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2016020202;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2016050400; // Moodle version.
 
 $plugin->component = 'mod_questionnaire';
 
-$plugin->release  = '3.1.0 (Build - 2016052000)';
+$plugin->release  = '3.1.1 (Build - 2016070700)';
 $plugin->maturity  = MATURITY_STABLE;
