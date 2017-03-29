d4b308ff672c104de20f2f8daff7f7a93fbc0e48
Mike Churchward <mike.churchward@poetgroup.org>
2017-03-29 11:29:42 -0400

    Setting up for 3.2.2 release.

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2016111102;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2016111103;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2016111500; // Moodle version.
 
 $plugin->component = 'mod_questionnaire';
 
-$plugin->release  = '3.2.1 (Build - 2017020200)';
+$plugin->release  = '3.2.2 (Build - 2017032900)';
 $plugin->maturity  = MATURITY_STABLE;
