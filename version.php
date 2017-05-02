95a0878da2b65ddfc0b1a4f72deee13ffeb5641b
Mike Churchward <mike.churchward@poetgroup.org>
2017-05-02 13:42:37 -0400

    POET - Setting up base 3.3 branch.

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2016111106;  // The current module version (Date: YYYYMMDDXX)
-$plugin->requires = 2016111500; // Moodle version.
+$plugin->version  = 2017050100;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2017042800; // Moodle version.
 
 $plugin->component = 'mod_questionnaire';
 
-$plugin->release  = '3.2.4 (Build - 2017042700)';
+$plugin->release  = '3.3.0 (Build - 2017050100)';
 $plugin->maturity  = MATURITY_STABLE;
