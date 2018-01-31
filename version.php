89d1cb632c6b9bcebc1e77a894bcd7f15135c5c0
Mike Churchward <mike.churchward@poetgroup.org>
2018-01-31 15:31:54 -0500

    Poet - Setting up for release 3.1.8.

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2016071102;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2016071103;  // The current module version (Date: YYYYMMDDXX)
 $plugin->requires = 2016050400; // Moodle version.
 
 $plugin->component = 'mod_questionnaire';
 
-$plugin->release  = '3.1.7 (Build - 2017042800)';
+$plugin->release  = '3.1.8 (Build - 2018013100)';
 $plugin->maturity  = MATURITY_STABLE;
