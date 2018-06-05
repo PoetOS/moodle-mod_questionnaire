d7bccf177fb3540c83d0256d63bb323f9bf808f1
Mark Sharp <m.sharp@chi.ac.uk>
2018-06-05 16:02:01 +0100

    CONTRIB-6982-M34_dataformat-api

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -444,4 +444,49 @@ class renderer extends \plugin_renderer_base {
 
         return $o;
     }
+
+    /**
+     * Returns a dataformat selection and download form
+     *
+     * @param string $label A text label
+     * @param moodle_url|string $base The download page url
+     * @param string $name The query param which will hold the type of the download
+     * @param array $params Extra params sent to the download page
+     * @param string $extrafields HTML for extra form fields
+     * @return string HTML fragment
+     */
+    public function download_dataformat_selector($label, $base, $name = 'dataformat', $params = array(), $extrafields = '') {
+
+        $formats = \core_plugin_manager::instance()->get_plugins_of_type('dataformat');
+        $options = array();
+        foreach ($formats as $format) {
+            if ($format->is_enabled()) {
+                $options[] = array(
+                    'value' => $format->name,
+                    'label' => get_string('dataformat', $format->component),
+                );
+            }
+        }
+        $hiddenparams = array();
+        foreach ($params as $key => $value) {
+            $hiddenparams[] = array(
+                'name' => $key,
+                'value' => $value,
+            );
+        }
+        $data = array(
+            'label' => $label,
+            'base' => $base,
+            'name' => $name,
+            'params' => $hiddenparams,
+            'options' => $options,
+            'extrafields' => $extrafields,
+            'sesskey' => sesskey(),
+            'submit' => get_string('download'),
+        );
+
+        return $this->render_from_template('mod_questionnaire/dataformat_selector', $data);
+    }
+
+
 }
