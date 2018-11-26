691857218c09aeaec68edcd81d04f2a554e37620
Paul Holden <pholden@greenhead.ac.uk>
2018-11-26 13:26:37 +0000

    CONTRIB-7412 improve response values when exporting "other" responses.

--- questionnaire.class.php
+++ questionnaire.class.php
@@ -3087,7 +3087,7 @@ class questionnaire {
                     $content = $choicesbyqid[$qid][$responserow->choice_id]->content;
                     if (preg_match('/^!other/', $content)) {
                         // If this has an "other" text, use it.
-                        $responsetxt = get_string('other', 'questionnaire');
+                        $responsetxt = preg_replace(["/^!other=/", "/^!other/"], ['', get_string('other', 'questionnaire')], $content);
                         $responsetxt1 = $responserow->response;
                     } else if (($choicecodes == 1) && ($choicetext == 1)) {
                         $responsetxt = $c.' : '.$content;
