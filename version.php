7d73edb919ab153bfea962a0032cdbf0dcef12d9
Mike Churchward <mike.churchward@poetgroup.org>
2020-06-25 11:12:14 -0400

    Initializing MOODLE_39_STABLE branch.

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2020011508;  // The current module version (Date: YYYYMMDDXX)
-$plugin->requires = 2018120300; // Moodle version (3.6).
+$plugin->version  = 2020062300;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2020061500; // Moodle version (3.6).
 
 $plugin->component = 'mod_questionnaire';
 
-$plugin->release  = '3.8.3 (Build - 2020062400)';
+$plugin->release  = '3.9.0 (Build - 2020062300)';
 $plugin->maturity  = MATURITY_STABLE;
