bb3854753f3f1823bda16d4aa58876152e5e0e6c
Mike Churchward <mike.churchward@poetgroup.org>
2020-06-26 11:25:18 -0400

    3.9 'officially' supported back to 3.7.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2020062301;  // The current module version (Date: YYYYMMDDXX)
-$plugin->requires = 2020061500; // Moodle version (3.6).
+$plugin->requires = 2019052006; // Moodle version (3.7).
 
 $plugin->component = 'mod_questionnaire';
 
