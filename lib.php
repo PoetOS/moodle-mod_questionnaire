2a4b23d3504330dec0fc9c7fe90d5e89717b376f
Luca Bösch <luca.boesch@bfh.ch>
2022-10-03 13:39:26 +0200

    Moodle 4.0 monologo.

--- lib.php
+++ lib.php
@@ -53,6 +53,8 @@ function questionnaire_supports($feature) {
             return true;
         case FEATURE_SHOW_DESCRIPTION:
             return true;
+        case FEATURE_MOD_PURPOSE:
+            return MOD_PURPOSE_COMMUNICATION;
 
         default:
             return null;
