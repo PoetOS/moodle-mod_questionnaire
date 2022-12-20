d202a7b7f34642a8cc8bc23a7acdc355f3d7b8b2
Mike Churchward <mike.churchward@poetopensource.org>
2022-12-20 21:58:00 +0100

    Nina fix show description once (#447)
    * Add version check for showing activity description or not
    
    Moodle 4.0 displays the description of activities by default.
    
    * Removing intro completely for 4.0.
    
    Co-authored-by: Nina Herrmann <nina.herrmann@uni-muenster.de>

--- view.php
+++ view.php
@@ -67,10 +67,7 @@ $PAGE->set_heading(format_string($course->fullname));
 echo $questionnaire->renderer->header();
 $questionnaire->page->add_to_page('questionnairename', format_string($questionnaire->name));
 
-// Print the main part of the page.
-if ($questionnaire->intro) {
-    $questionnaire->page->add_to_page('intro', format_module_intro('questionnaire', $questionnaire, $cm->id));
-}
+// No need to print out intro in Moodle 4 and above.
 
 $cm = $questionnaire->cm;
 $currentgroupid = groups_get_activity_group($cm);
