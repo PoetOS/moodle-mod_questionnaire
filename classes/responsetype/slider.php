1066a7324e06733344ac32d742e25a57dcabc5e2
Joseph Rézeau <joseph@rezeau.org>
2023-06-24 22:46:42 +0200

    Fixed 2 minor codecheck & php doc errors
    Signed-off-by: Joseph Rézeau <joseph@rezeau.org>

--- classes/responsetype/slider.php
+++ classes/responsetype/slider.php
@@ -52,6 +52,11 @@ class slider extends numericaltext {
         return $answers;
     }
 
+    /**
+     * Provide the feedback scores for all requested response id's. This should be provided only by questions that provide feedback.
+     * @param array $rids
+     * @return array | boolean
+     */
     public function get_feedback_scores(array $rids) {
         global $DB;
         $rsql = '';
@@ -62,7 +67,7 @@ class slider extends numericaltext {
             $rsql = ' AND response_id ' . $rsql;
         }
         $sql = 'SELECT response_id as rid, response AS score ' .
-            'FROM {'.$this->response_table().'} r ' .            
+            'FROM {'.$this->response_table().'} r ' .
             'WHERE r.question_id= ? ' . $rsql . ' ' .
             'ORDER BY response_id ASC';
         return $DB->get_records_sql($sql, $params);
