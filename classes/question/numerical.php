45647902a9446831162db4d4b93c871b5d431a34
CPASimUSante <dechepy.olivier@chu-amiens.fr>
2024-02-23 19:52:23 +0100

    fix PHP8 warning due to wrong initialization format (#539)

--- classes/question/numerical.php
+++ classes/question/numerical.php
@@ -83,7 +83,7 @@ class numerical extends question {
         // Numeric.
         $questiontags = new \stdClass();
         $precision = $this->precise;
-        $a = '';
+        $a = new \StdClass();
         if (isset($response->answers[$this->id][0])) {
             $mynumber = $response->answers[$this->id][0]->value;
             if ($mynumber != '') {
