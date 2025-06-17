26eb7759a23e2ffd4f2a67078f0f51305382bf08
Diego Monroy <diego.monroy@moodle.com>
2025-06-17 17:16:46 +0200

    DEF-4027: bump version

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022121602;  // The current module version (Date: YYYYMMDDXX).
+$plugin->version = 2022121603;  // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2024042200.00; // Moodle version (4.4.0).
 
 $plugin->component = 'mod_questionnaire';
 
-$plugin->release = '4.4.0 (Build - 2024082900)';
+$plugin->release = '4.5.0 (Build - 2024082901)';
 $plugin->maturity = MATURITY_BETA;
