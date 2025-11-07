f43fd1d8bab2491ae23db86cefa84d97782e38cf
Mike Churchward <mike.churchward@poetopensource.org>
2025-11-07 09:53:30 -0500

    Fixing stuff.

--- classes/question/question.php
+++ classes/question/question.php
@@ -1035,10 +1035,10 @@ abstract class question {
      * Override this, or any of the internal methods, to provide specific form data for editing the question type.
      * The structure of the elements here is the default layout for the question form.
      * @param edit_question_form $form The main moodleform object.
-     * @param questionnaire $questionnaire The questionnaire being edited.
+     * @param \mod_questionnaire\questionnaire $questionnaire The questionnaire being edited.
      * @return bool
      */
-    public function edit_form(edit_question_form $form, questionnaire $questionnaire) {
+    public function edit_form(edit_question_form $form, \mod_questionnaire\questionnaire $questionnaire) {
         $mform =& $form->_form;
         $this->form_header($mform);
         $this->form_name($mform);
