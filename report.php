dd285d9b6a5dcf5d2a949eb5c7cbf93f219388eb
Mike Churchward <mike.churchward@poetopensource.org>
2025-11-10 14:28:03 -0500

    Add group selector to by response report (#675)
    * Cleaning up warnings.
    
    * Add group selector to individual report (#674)
    
    * Add group filter dropdown to List of responses
    
    * Ensuring by response page is displayed when no responses.
    
    ---------
    
    Co-authored-by: Iñigo Zendegi Urzelai <izendegi@mondragon.edu>
    
    ---------
    
    Co-authored-by: Iñigo Zendegi Urzelai <izendegi@mondragon.edu>

--- report.php
+++ report.php
@@ -792,6 +792,13 @@ switch ($action) {
             }
         }
 
+        // Add group filter dropdown.
+        if ($groupmode > 0) {
+            $groupselect = groups_print_activity_menu($cm, $url->out(), true);
+            $questionnaire->page->add_to_page('respondentinfo', $groupselect);
+            $currentgroupid = groups_get_activity_group($cm);
+        }
+
         if ($byresponse || $rid) {
             // Available group modes (0 = no groups; 1 = separate groups; 2 = visible groups).
             if ($groupmode > 0) {
@@ -830,13 +837,7 @@ switch ($action) {
             $rid = $rids[0];
         }
 
-        if ($noresponses) {
-            $questionnaire->page->add_to_page(
-                'respondentinfo',
-                get_string('group') . ' <strong>' .
-                    groups_get_group_name($currentgroupid) . '</strong>: ' . get_string('noresponses', 'questionnaire')
-            );
-        } else if ($outputtarget == 'pdf') {
+        if ($outputtarget == 'pdf') {
             $pdf = questionnaire_report_start_pdf();
             if ($currentgroupid > 0) {
                 $groupname = get_string('group') . ': <strong>' . groups_get_group_name($currentgroupid) . '</strong>';
@@ -854,6 +855,14 @@ switch ($action) {
             @$pdf->Output(clean_param($questionnaire->name, PARAM_FILE), 'D');
             error_reporting($errorreporting);
         } else { // Default to HTML.
+            if ($noresponses) {
+                $questionnaire->page->add_to_page(
+                    'respondentinfo',
+                    get_string('group') . ' <strong>' .
+                        groups_get_group_name($currentgroupid) . '</strong>: ' . get_string('noresponses', 'questionnaire')
+                );
+            }
+
             // Print the page header.
             $PAGE->set_title(get_string('questionnairereport', 'questionnaire'));
             $PAGE->set_heading(format_string($course->fullname));
