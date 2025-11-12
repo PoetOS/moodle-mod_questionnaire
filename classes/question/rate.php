bf725581ebc57ddb8064db25afe06faad8570959
Luca Bösch <luca.boesch@bfh.ch>
2025-11-12 18:35:52 +0100

    Addressing $response instead of $data, resolves #624.

--- classes/question/rate.php
+++ classes/question/rate.php
@@ -209,8 +209,8 @@ class rate extends question {
         if ($blankquestionnaire) {
             $disabled = ' disabled="disabled"';
         }
-        if (!empty($data) && (!isset($data->{'q' . $this->id}) || !is_array($data->{'q' . $this->id}))) {
-            $data->{'q' . $this->id} = [];
+        if (!empty($response) && (!isset($response->{'q' . $this->id}) || !is_array($response->{'q' . $this->id}))) {
+            $response->{'q' . $this->id} = [];
         }
 
         // Check if rate question has one line only to display full width columns of choices.
