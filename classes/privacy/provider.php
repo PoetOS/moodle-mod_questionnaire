9fa2a3e437902be99445263910a3a3a071ad30c9
Mike Churchward <mike.churchward@poetopensource.org>
2026-02-19 09:22:57 -0500

    New code checker requirement.

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -31,8 +31,8 @@ use core_privacy\local\request\approved_userlist;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements
-    \core_privacy\local\request\core_userlist_provider,
     \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\core_userlist_provider,
     \core_privacy\local\request\plugin\provider {
     /**
      * Returns meta data about this system.
