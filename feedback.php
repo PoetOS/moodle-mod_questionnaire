1aa94f42fde1deed0981ab6ba2e19245da616d0f
Mike Churchward <mike.churchward@poetopensource.org>
2026-04-07 10:35:56 -0400

    Simplifying multi line if.

--- feedback.php
+++ feedback.php
@@ -138,10 +138,8 @@ if ($settings = $feedbackform->get_data()) {
     // Handle the edit feedback sections action.
     if (isset($settings->buttongroup['feedbackeditbutton'])) {
         // Create a single section for Global Feedback if not existent.
-        if (!($firstsection = $DB->get_field('questionnaire_fb_sections', 'MIN(section)',
-                ['surveyid' => $questionnaire->surveyid()]))) {
-            $firstsection = 0;
-        }
+        $firstsection =
+            $DB->get_field('questionnaire_fb_sections', 'MIN(section)', ['surveyid' => $questionnaire->surveyid()]) ?: 0;
         if (($sdata->feedbacksections > 0) && ($firstsection == 0)) {
             if ($sdata->feedbacksections == 1) {
                 $sectionlabel = get_string('feedbackglobal', 'questionnaire');
