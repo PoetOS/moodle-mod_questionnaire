4d3a39da522628ff9751806e04ebe19d1dde1486
Mike Churchward <mike.churchward@poetopensource.org>
2026-06-26 10:42:14 -0400

    Test

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2026050100.02;
-$plugin->requires  = 2025041400.00; // Moodle 5.0 and up.
+$plugin->requires  = 2024100700.00; // Moodle 4.5 and up.
 
 $plugin->component = 'mod_questionnaire';
 
