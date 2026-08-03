019e114cd53a4719112eef9f6250e63a6130196a
Cameron Ball <cameronball@catalyst-au.net>
2026-08-03 09:24:29 +0800

    GHI769 Fix custom completion test compatibility

--- tests/custom_completion_test.php
+++ tests/custom_completion_test.php
@@ -209,13 +209,12 @@ class custom_completion_test extends \advanced_testcase {
         // Build a mock cm_info instance.
         $mockcminfo = $this->getMockBuilder(cm_info::class)
             ->disableOriginalConstructor()
-            ->onlyMethods(['__get'])
+            ->onlyMethods(['get_custom_data'])
             ->getMock();
 
-        // Mock the return of magic getter for the customdata attribute.
+        // Mock the return of the customdata getter.
         $mockcminfo->expects($this->any())
-            ->method('__get')
-            ->with('customdata')
+            ->method('get_custom_data')
             ->willReturn($customdataval);
 
         $customcompletion = new custom_completion($mockcminfo, 1);
